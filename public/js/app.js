167a2136a91375e5d9642eb7f8f076cf763b208a
Hemil <hemilparmar1005@gmail.com>
2020-12-17 17:40:57 +0530

    Romove unneccesary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-console.log('Client side javascripy file is loaded')
+// console.log('Client side javascripy file is loaded')
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
